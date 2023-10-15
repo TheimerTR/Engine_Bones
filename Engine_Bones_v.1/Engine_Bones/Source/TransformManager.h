4f96c68d186e12c0c82a8525bbfde9a41f2d3142
SoniaOL <99950351+SoniaOL@users.noreply.github.com>
2023-10-15 17:33:11 +0200

    transform cambio matriz

--- Engine_Bones_v.1/Engine_Bones/Source/TransformManager.h
+++ Engine_Bones_v.1/Engine_Bones/Source/TransformManager.h
@@ -7,16 +7,22 @@ private:
 	float3 position; 
 	Quat rotation; 
 	float3 scale; 
+	float4x4 transform;
 
 public: 
-	void SetPosition(float3 position) { this->position = position; };
-	void SetRotation(Quat rotation) { this->rotation = rotation; };
-	void SetScale(float3 scale) { this->scale = scale; };
+	void SetPosition(float3 position) { this->position = position; UpdateMatrix(); };
+	void SetRotation(Quat rotation) { this->rotation = rotation; UpdateMatrix(); };
+	void SetScale(float3 scale) { this->scale = scale; UpdateMatrix(); };
 
 	float3 inline GetPosition() const { return this->position; };
 	Quat inline GetRotation() const { return this->rotation; };
 	float3 inline GetScale() const { return this->scale; };
 
+private: 
+
+	void UpdateMatrix() {
+		transform = float4x4::FromTRS(position, rotation, scale);
+	}
 	
 
 };
